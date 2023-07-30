898ac1c3f37e2c31de3cd2e40bf0b073e3cd8ea9
Suleman <sulekhan111@gmail.com>
2023-07-30 10:53:29 +0200

    rerere

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import random
 
 move = ["", "Rock", "Paper", "Scissors"]
-# test commit 2 adassdas
-# t
+# test commit 
+# tester saasdsd
 user_move = input("Type 1 for Rock, 2 for Paper and 3 for Scissors.\nEnter your move: ")
 user_move = int(user_move)
 
